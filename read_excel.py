7da579cd914ceda311b17cc0da0507573a08438a
LuisTabile <luistabile@github.com>
2023-07-03 16:28:09 -0300

    :sparkles: feat: now user write the name of the file

--- read_excel.py
+++ read_excel.py
@@ -8,59 +8,68 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import re
 
-# Abre o arquivo xlsx
-df = pd.read_excel('Teste.xlsx')
+# Prompt the user for input and output file names
+input_file = input("Digite o nome do arquivo xlsx, junto com a extensão. Exemplo: Teste.xlsx: ")
+output_file = input("Digite o nome do novo arquivo xlsx. Se deixar em branco, irá sobrescrever o arquivo de entrada: ")
 
-# Configura o driver do Chrome
+# Check if the output file name is empty
+if not output_file:
+    output_file = input_file
+    print(f"Aviso: O arquivo de saída será sobrescrito com o mesmo nome do arquivo de entrada: {output_file}")
+
+# Open the input Excel file
+df = pd.read_excel(input_file)
+
+# Configure the Chrome driver
 webdriver_service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=webdriver_service)
 
 for idx, row in df.iterrows():
-    # Gera a URL
+    # Generate the URL
     url = f'https://www.digikey.com.br/pt/products/detail/{row["codigo"]}'
-    print(f"Processando o URL: {url}")
+    print(f"Processing URL: {url}")
 
-    # Carrega a página
+    # Load the page
     driver.get(url)
 
     try:
-        # Espera até 10 segundos até que a tabela de preços seja carregada na página
+        # Wait up to 10 seconds until the price table is loaded on the page
         table = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, "table.MuiTable-root.tss-1w92vj0-table.css-u6unfi"))
         )
 
-        # Extrai o código HTML da tabela
+        # Extract the HTML code of the table
         table_html = table.get_attribute('outerHTML')
 
-        # Cria o objeto BeautifulSoup
+        # Create the BeautifulSoup object
         soup = BeautifulSoup(table_html, 'html.parser')
 
-        # Encontra todas as linhas da tabela
+        # Find all rows in the table
         rows = soup.find('tbody').find_all('tr')
 
-        # Para cada linha na tabela
+        # For each row in the table
         for row in rows:
-            # Encontra todas as células na linha
+            # Find all cells in the row
             cells = row.find_all('td')
 
-            # Verifica se a primeira célula contém '1000'
+            # Check if the first cell contains '1.000'
             if len(cells) > 0 and cells[0].text.strip() == '1.000':
-                # Remove o símbolo de dólar e espaços do texto
+                # Remove the dollar symbol and spaces from the text
                 price_text = cells[1].text.replace('$', '').strip()
-                # Substitui a vírgula pelo ponto na string
+                # Replace the comma with a dot in the string
                 price_text = price_text.replace(',', '.')
-                # Converte o preço para um número
+                # Convert the price to a number
                 price = float(price_text)
-                # Atualiza o preço na linha do DataFrame
+                # Update the price in the DataFrame row
                 df.at[idx, 'preco'] = price
-                print(f"Preço atualizado para: {price}")
+                print(f"Updated price: {price}")
                 break
 
     except Exception as e:
-        print(f"Erro ao processar o URL: {url}, erro: {e}")
+        print(f"Error processing URL: {url}, error: {e}")
 
-# Fecha o navegador
+# Close the browser
 driver.quit()
 
-# Salva o DataFrame alterado de volta para o arquivo xlsx
-df.to_excel('Teste.xlsx', index=False)
+# Save the modified DataFrame back to the output Excel file
+df.to_excel(output_file, index=False)
